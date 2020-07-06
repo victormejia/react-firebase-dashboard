67f7f87eac8ffdd97e6b8c0f4b0ee30e8bbcea78
Victor Mejia <victor.a.mejia@gmail.com>
2020-07-06 07:20:40 -0700

    :zap: initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <div className="app">
+        <div className="ui grid container"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h2>The Grid</h2>
+    </header>
+  )
+}
+
+export default Header;
