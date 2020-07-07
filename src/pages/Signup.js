3e0ced2a639578cd56978d9693700122d5563b24
Victor Mejia <victor.a.mejia@gmail.com>
2020-07-07 07:11:49 -0700

    02-02: create user

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,39 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { signup } from '../firebase/auth';
 
 function Signup() {
+  const { register, handleSubmit, reset } = useForm();
+  const [isLoading, setLoading] = useState(false);
+
+  const onSubmit = async (data) => {
+    setLoading(true);
+    try {
+      await signup(data);
+      reset();
+    } catch(error) {
+      console.log(error);
+    }
+    setLoading(false);
+  }
+
+  const formClassName = `ui form ${isLoading ? 'loading' : ''}`;
+
   return (
     <div className="login-container">
       <div className="ui card login-card">
         <div className="content">
-          <form className="ui form">
+          <form className={formClassName} onSubmit={handleSubmit(onSubmit)}>
             <div className="two fields">
-              <div className="field">
+              <div className="field" >
                 <label>
                   First Name
                   <input
                     type="text"
                     name="firstName"
                     placeholder="First Name"
+                    ref={register}
                   />
                 </label>
               </div>
               <div className="field">
                 <label>
                   Last Name
-                  <input type="text" name="lastName" placeholder="Last Name" />
+                  <input
+                    type="text"
+                    name="lastName"
+                    placeholder="Last Name"
+                    ref={register}
+                  />
                 </label>
               </div>
             </div>
             <div className="field">
               <label>
                 Email
-                <input type="email" name="email" placeholder="Email" />
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  ref={register}
+                />
               </label>
             </div>
             <div className="field">
               <label>
                 Password
-                <input type="password" name="password" placeholder="Password" />
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  ref={register}
+                />
               </label>
             </div>
             <button className="ui primary button login" type="submit">
