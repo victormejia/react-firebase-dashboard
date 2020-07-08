4ac78a8e643f07f3df3ae523f8bf2affe57d01d6
Victor Mejia <victor.a.mejia@gmail.com>
2020-07-07 19:02:54 -0700

    02-05: user logout

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { logout } from './firebase/auth';
+import { useHistory } from 'react-router-dom';
+import { useSession } from './firebase/UserProvider';
 
 function Header() {
+  const history = useHistory();
+  const { user } = useSession();
+
+  const logoutUser = async () => {
+    await logout();
+    history.push('/signup');
+  };
+
   return (
     <header>
       <h2>The Grid</h2>
+      {!!user &&
+      <button className="ui secondary button logout" onClick={logoutUser}>
+        LOGOUT
+      </button>}
     </header>
   )
 }
