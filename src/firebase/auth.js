c40cd39e6e5ea03702978c5ab800ced0f73f6be3
Victor Mejia <victor.a.mejia@gmail.com>
2020-07-07 18:36:15 -0700

    02-04: redirect on user signup

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -3,5 +3,7 @@ import 'firebase/auth';
 
 export const signup = async ({ firstName, lastName, email, password }) => {
   const resp = await firebase.auth().createUserWithEmailAndPassword(email, password);
-  await resp.user.updateProfile({ displayName: `${firstName} ${lastName}`});
+  const user = resp.user;
+  await user.updateProfile({ displayName: `${firstName} ${lastName}`});
+  return user;
 }
