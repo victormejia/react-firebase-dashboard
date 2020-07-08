a68f5411c65d65e57e5602787183f3ad30fcc5aa
Victor Mejia <victor.a.mejia@gmail.com>
2020-07-07 23:09:12 -0700

    02-06: user login

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -2,12 +2,22 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 export const signup = async ({ firstName, lastName, email, password }) => {
-  const resp = await firebase.auth().createUserWithEmailAndPassword(email, password);
+  const resp = await firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password);
   const user = resp.user;
-  await user.updateProfile({ displayName: `${firstName} ${lastName}`});
+  await user.updateProfile({ displayName: `${firstName} ${lastName}` });
   return user;
-}
+};
 
 export const logout = () => {
   return firebase.auth().signOut();
-}
+};
+
+export const login = async ({ email, password }) => {
+  const resp = await firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password);
+
+  return resp.user;
+};
