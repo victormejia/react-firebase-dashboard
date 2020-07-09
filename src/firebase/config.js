4b66b4a2219d30c10c817772d053b5eb6e412b31
Victor Mejia <victor.a.mejia@gmail.com>
2020-07-08 23:25:11 -0700

    03-04: create user document

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 firebase.initializeApp({
   apiKey: process.env.REACT_APP_API_KEY,
@@ -6,6 +7,8 @@ firebase.initializeApp({
   databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 });
 console.log(firebase.app().options);
+
+export const firestore = firebase.firestore();
