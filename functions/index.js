15e2c8cc75fc0377260ff32c1bccbe3ceea055b6
Victor Mejia <victor.a.mejia@gmail.com>
2020-07-11 13:47:36 -0700

    06-03: cloud functions - auth trigger

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,35 @@
 const functions = require('firebase-functions');
+const nodemailer = require('nodemailer');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// env vars
+const gmailEmail = functions.config().gmail.email;
+const gmailPassword = functions.config().gmail.password;
+
+// create node mailer transport
+const mailTransport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: gmailEmail,
+    pass: gmailPassword,
+  },
+});
+
+async function sendWelcomeEmail(email) {
+  const mailOptions = {
+    from: 'The Grid Notifier <thegrid.notifier@gmail.com>',
+    to: email,
+    subject: 'Welcome to The Grid!',
+    text: `Hey ${email}! Welcome to The Grid, the elite team of secret agents.`,
+  };
+
+  await mailTransport.sendMail(mailOptions);
+
+  console.log('New welcome email sent to:', email);
+
+  return null;
+}
+
+exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
+  const email = user.email;
+  return sendWelcomeEmail(email);
+});
